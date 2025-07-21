2828fc7687c514960cfe3f1661ced49c540c9dbe
MosingiA <ondicho33@gmail.com>
2025-07-21 15:22:37 +0300

    final changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-// src/main.jsx
+// main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import "./index.css"; 
+import "./index.css"; // Assuming you have some global styles
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
